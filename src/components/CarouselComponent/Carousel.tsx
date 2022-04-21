8d8d5b20a61f5cfc7ac039a26ac8462eb0e77829
J.Daniel <juandaniel9619@gmail.com>
2022-04-21 11:51:21 -0400

    fixed Carousel

--- src/components/CarouselComponent/Carousel.tsx
+++ src/components/CarouselComponent/Carousel.tsx
@@ -22,11 +22,11 @@ const Carousel:any = function({nfts}:any){
     //TODO fix a bug that makes the timer render infinitely in a loop
     },[currentSlide, length])
      useEffect(() => { //this allows the carousel autoplay
-         setInterval(() => {
+        const interval = setInterval(() => {
              moveRight();
          }, 5500);
          return ()=>{
-             clearInterval()
+             clearInterval(interval)
 
          }
 
